159d0317c71075c3943479f80e222f2373e3854c
Rich FitzJohn <rich.fitzjohn@gmail.com>
2020-05-27 16:14:37 +0100

    Add odin code

---
+++ inst/examples/sir.R
@@ -0,0 +1,25 @@
+update(S) <- S - n_SI
+update(I) <- I + n_SI - n_IR
+update(R) <- R + n_IR
+
+initial(S) <- S0
+initial(I) <- I0
+initial(R) <- 0
+
+p_SI <- 1 - exp(-beta * I / N)
+p_IR <- 1 - exp(-gamma)
+
+n_SI <- rbinom(S, p_SI * dt)
+n_IR <- rbinom(I, p_IR * dt)
+
+output(incid) <- n_SI
+output(day) <- step * dt
+
+steps_per_day <- user(4)
+S0 <- user(1000)
+I0 <- user(10)
+beta <- user(0.2)
+gamma <- user(0.1)
+
+dt <- 1 / steps_per_day
+N <- S + I + R
